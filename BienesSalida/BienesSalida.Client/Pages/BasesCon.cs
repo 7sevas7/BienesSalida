d6121e8785f17255f71efc7221347a4049aec86c
Sebas <s3v4s51@gmail.com>
2025-06-04 16:15:00 -0600

    Cambios consulta BD

--- BienesSalida/BienesSalida.Client/Pages/BasesCon.cs
+++ BienesSalida/BienesSalida.Client/Pages/BasesCon.cs
@@ -1,75 +1,50 @@
-﻿using BienesSalida.Share.Models;
-using Microsoft.Data.SqlClient;
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Threading.Tasks;
 
 namespace BienesSalida.Components.Pages
 {
-    public class BasesCon : IDisposable
+    public class BasesCon
     {
-        private SqlConnection db1Users, db2SBienes;
-        public int idEmp;
-        public string nombreEmp;
-        public Boolean res = false;
-
-        // Constructor que inicializa las conexiones
-        public BasesCon()
+        public async Task consultaAsync()
         {
-            db1Users = new SqlConnection("Server=172.16.1.42\\COMPAC; Database=expediente_personas2018; User Id=usuSalidasBienes; Password=2E<+AuU3v$Cx=R~7$;");
-            //db2SBienes = new SqlConnection("Server=192.168.1.10\\COMPAC; Database=prueba2; User Id=sa; Password=compac$1;");
-
-            db1Users.Open();
-            //db2SBienes.Open();
-        }
-
-        public async Task ObtenerUsuarios(string rfc, string pass)
-        {
-            string query = "SELECT id_empleado, nombre_completo FROM vw_acceso WHERE RFC = '"+rfc+"' and pass = '"+pass+"'";
-
-            using (SqlCommand command = new SqlCommand(query, db1Users))
-            using (SqlDataReader reader = await command.ExecuteReaderAsync()) // Usa versión async
+            var builder = new SqlConnectionStringBuilder
             {
-                if (await reader.ReadAsync()) // Solo obtiene el primer registro
+                DataSource = "172.16.1.42",
+                UserID = "usuSalidasBienes",
+                Password = "2E<+AuU3v$Cx=R~7$",
+                InitialCatalog = "expediente_personas2018",
+                TrustServerCertificate = true // Agregar esta línea
+
+            };
+
+            var connectionString = builder.ConnectionString;
+
+            try
+            {
+                await using var connection = new SqlConnection(connectionString);
+                await connection.OpenAsync();
+
+                Console.WriteLine("Conexión establecida correctamente.");
+
+                var sql = "SELECT id_empleado, nombre_completo FROM vw_acceso";
+                //var sql = "SELECT id_empleado, nombre_completo FROM vw_acceso WHERE RFC = '"+rfc+"' and pass = '"+pass+"'";
+                await using var command = new SqlCommand(sql, connection);
+                await using var reader = await command.ExecuteReaderAsync();
+
+                while (await reader.ReadAsync())
                 {
-                    idEmp = Convert.ToInt32(reader["id_empleado"]);
-                    nombreEmp = reader["nombre_completo"].ToString();
-                    Console.WriteLine(nombreEmp);
-                    res = true;
-                }
-                else
-                {
-                    res = false;
+                    Console.WriteLine("ID: {0}, Nombre: {1}", reader.GetInt32(0), reader.GetString(1));
                 }
             }
-
-        }
-
-        //// Método para consultar Productos desde la segunda base de datos
-        //public void ObtenerProductos()
-        //{
-        //    string query = "SELECT * FROM Productos";
-        //    using (SqlCommand command = new SqlCommand(query, db2SBienes))
-        //    using (SqlDataReader reader = command.ExecuteReader())
-        //    {
-        //        while (reader.Read())
-        //        {
-        //            Console.WriteLine($"Producto: {reader["Nombre"]}");
-        //        }
-        //    }
-        //}
-
-        // Liberar las conexiones correctamente
-        public void Dispose()
-        {
-            if (db1Users != null)
+            catch (SqlException sqlEx)
             {
-                db1Users.Close();
-                db1Users.Dispose();
+                Console.WriteLine("Error en SQL Server: " + sqlEx.Message);
             }
-
-            //if (db2SBienes != null)
-            //{
-            //    db2SBienes.Close();
-            //    db2SBienes.Dispose();
-            //}
+            catch (Exception e)
+            {
+                Console.WriteLine("Error general: " + e.Message);
+            }
         }
     }
 }
