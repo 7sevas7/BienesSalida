34922a782a0a2e09027dcc3fc9a971dfe52be65c
Sebas <s3v4s51@gmail.com>
2025-06-06 10:11:49 -0600

    Actualización de script

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,5 @@
+-- BASE SISTEMA DE VIENES--
+
 create database SistemaBien
 use SistemaBien
 
@@ -27,3 +29,44 @@ FOREIGN KEY (idUserSBFK) REFERENCES Usuarios(idUserSB));
 
 select * from Usuarios;
 select * from Salida;
+
+
+GO
+CREATE PROCEDURE InsercLogin
+    @ID_UserEU INT,
+    @Nombre VARCHAR(250),
+    @Roll VARCHAR(250)
+AS
+BEGIN
+    IF NOT EXISTS (SELECT * FROM Usuarios WHERE idUserEU = @ID_UserEU AND Nombre = @Nombre)
+    BEGIN
+        INSERT INTO Usuarios (idUserEU, Nombre, Roll)
+        VALUES (@ID_UserEU, @Nombre, @Roll);
+    END; 
+END;
+GO
+
+--DROP PROCEDURE InsercLogin;
+
+
+
+-- BASE USUARIOS --
+
+use expediente_personas2018;
+
+SELECT * FROM vw_acceso WHERE RFC = 'LESA480608' and pass = 'ND05'
+SELECT id_empleado, nombre_completo FROM vw_acceso WHERE RFC = 'LESA480608' and pass = 'ND05'
+
+GO
+CREATE PROCEDURE ConsultaSistemaDeBienes
+    @rfc VARCHAR(250),
+    @pass VARCHAR(250)
+AS
+BEGIN
+    SELECT id_empleado, nombre_completo FROM vw_acceso WHERE RFC = @rfc AND pass = @pass;
+END;
+GO
+
+EXEC ConsultaSistemaDeBienes 'LESA480608', 'ND05';
+
+--DROP PROCEDURE ConsultaSistemaDeBienes;
