779a62820749c2693f97fdd7c8c96219d5a3b46a
Sebas <s3v4s51@gmail.com>
2025-06-18 14:17:49 -0600

    Implementación de estatus

--- BienesSalida.Share/ModelsPost/SolicitudBien.cs
+++ BienesSalida.Share/ModelsPost/SolicitudBien.cs
@@ -12,26 +12,18 @@ namespace BienesSalida.Share.ModelsPost
     {
         public string? Nombre { get; set; }
         public string? Fecha { get; set; }
-
         [Required(ErrorMessage = "Área requerida.")]
         public string? Area { get; set; }
-
         [Required(ErrorMessage = "Encargado de área requerido.")]
         public string? EncargadoArea { get; set; }
-
         public int? Numero { get; set; }
-
-
         public int? NumeroInventario { get; set; }
-
         public string? Descripcion { get; set; }
-
         [Required(ErrorMessage = "Motivo de salida requerido.")]
         public string? MotivoSalida { get; set; }
-
         [Required(ErrorMessage = "Observaciones requerido.")]
         public string? Observaciones { get; set; }
         public Producto producto { get; set; } = new Producto();
-
+        public string? estatus { get; set; }
     }
 }
